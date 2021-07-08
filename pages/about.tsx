94568dd2c06b6acfb4e7b29d6b0d758904aad2e0
shubham sananse <shubham.sananse@grofers.com>
2021-07-08 23:12:52 +0530

    removed about tsx comments

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,11 +4,7 @@ import Layout from "../components/Layout";
 const AboutPage = () => (
     <Layout title="About" description="About Shubham Sananse" relativeCanonicalURL="/about">
         <Heading>About</Heading>
-        <Text fontSize="lg">
-            {/* Hey and welcome! I’m a Software developer working in a fin-tech startup called Refyne in India. In my free
-            time I make things in Python, Node js or Golang. */}
-            work in progress...
-        </Text>
+        <Text fontSize="lg">work in progress...</Text>
     </Layout>
 );
 
